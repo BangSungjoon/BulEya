48981559c8f95e2153702e50b480f26dc9cff897
gmltn0907 <gmltn0907@naver.com>
2025-03-17 11:13:52 +0900

    DESIGN : 메인 컬러 추가 jira ref: #S12P21A204-100

--- mobile/app/src/main/java/com/ssafy/jangan_mobile/ui/theme/Color.kt
+++ mobile/app/src/main/java/com/ssafy/jangan_mobile/ui/theme/Color.kt
@@ -1,21 +1,22 @@
 package com.ssafy.jangan_mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
+//
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-val primaryColor = Color(0xFFEE5B22)
-val orangeColor = Color(0xFFFF7A27)
-val lightColor = Color(0xFFFFF0EA)
-val greyBackgroundColor = Color(0xFFF3F3F3)
-val polishedSteelColor = Color(0xFF9C9C9C)
-val titleTextColor = Color(0xFF000000)
-val contentTextColor = Color(0xFF53443A)
-val whiteBackgroundColor = Color(0xFFFFFFFF)
-val formFillColor = Color(0xFFF0F5FA)
+val primaryColor = Color(0xFF8AEA52)
+val system_red = Color(0xFFF83A8A)
+val system_yellow = Color(0xFFFBE205)
+val system_grey = Color(0xFF34A853)
+val system_blue = Color(0xFF4285F4)
+val grey600 = Color(0xFF1B1B1D)
+val grey500 = Color(0xFF424242)
+val grey400 = Color(0xFF9BA0A5)
+val grey300 = Color(0xFFE9EEf6)
+val grey200 = Color(0xFFF8FAFD)
