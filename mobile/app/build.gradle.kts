aa2cbb9e9c6897c63df46d2963f0221fa889ff84
권정우 <kiw970923@naver.com>
2025-03-20 10:29:04 +0900

    CHORE: Navigation 작성 및 디렉토리 구조 정의

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -52,6 +52,8 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("androidx.navigation:navigation-compose:2.8.9")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ mobile/app/src/main/java/com/ssafy/jangan_mobile/ui/navigation/AppNavigation.kt
@@ -0,0 +1,17 @@
+package com.ssafy.jangan_mobile.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ){
+
+    }
+}
