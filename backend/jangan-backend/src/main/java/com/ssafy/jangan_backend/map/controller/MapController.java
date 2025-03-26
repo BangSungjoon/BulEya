4d35dac990a5be06df21ec913ab9088e7870203b
홍승찬 <hsch19@naver.com>
2025-03-26 14:13:31 +0900

    FIX: URL snakeCase로 변경됨에 따라 @RequestBody수정
    URL 쿼리스트링이 ?stationId에서 station_id로 변경됨에 따라 @RequestBody에 명시적으로 설정해줌

--- backend/jangan-backend/src/main/java/com/ssafy/jangan_backend/map/controller/MapController.java
+++ backend/jangan-backend/src/main/java/com/ssafy/jangan_backend/map/controller/MapController.java
@@ -18,13 +18,13 @@ import java.util.List;
 public class MapController {
     private final MapService mapService;
     @GetMapping("/mobile")
-    public BaseResponse getMapsForMobile(@RequestParam int stationId) {
+    public BaseResponse getMapsForMobile(@RequestParam("station_id") int stationId) {
         List<ResponseMobileMapDto> list = mapService.getMapsForMobile(stationId);
         return BaseResponse.ok(list);
     }
 
     @GetMapping("/admin")
-    public BaseResponse getMapsForWebAdmin(@RequestParam int stationId) {
+    public BaseResponse getMapsForWebAdmin(@RequestParam("station_id") int stationId) {
         List<ResponseWebAdminMapDto> list = mapService.getMapsForWebAdmin(stationId);
         return BaseResponse.ok(list);
     }
