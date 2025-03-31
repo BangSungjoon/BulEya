4eb90d0084c98f4b9800ed48ef43e1924597825c
Sungjoon <happypigs7.dev@gmail.com>
2025-03-31 15:09:04 +0900

    FEAT: 지도에 간선과 노드 매핑
    jira ref: #S12P21A204-192

--- web/src/pages/MapPage.jsx
+++ web/src/pages/MapPage.jsx
@@ -16,17 +16,31 @@ export default function MapPage() {
 
   const [floorDataList, setFloorDataList] = useState([]) // 전체 응답 저장
   const [selectedFloor, setSelectedFloor] = useState(null) // 선택된 층 번호
+  const [selectedIcon, setSelectedIcon] = useState(null) // IconBox 관련
 
   // 지도 이미지 불러오는 API 호출
   useEffect(() => {
     const loadFloorData = async () => {
       try {
         const response = await fetchMapImage(stationId)
+        console.log('응답:', response.data)
+        const result = response.data.result
         // is_success에 따른 에러 처리 필요
-        const data = response.data.result
+        // const data = response.data.result
 
-        setFloorDataList(data)
-        setSelectedFloor(data[0].floor) // 첫 번째 층이 기본
+        // setFloorDataList(data)
+        // setSelectedFloor(data[0].floor) // 첫 번째 층이 기본
+
+        const parsedData = result.map((data) => ({
+          floor: data.floor, // 층 정보가 이 안에 있다고 가정
+          image_url: data.image_url,
+          beacon_list: data.beacon_list,
+          edge_list: data.edge_list,
+        }))
+        console.log('파싱된 데이터:', parsedData)
+
+        setFloorDataList(parsedData)
+        setSelectedFloor(parsedData[0].floor) // 첫 번째 층이 기본
       } catch (error) {
         console.error('지도 데이터 로드 실패: ', error)
       }
@@ -36,17 +50,20 @@ export default function MapPage() {
   }, [stationId])
 
   // 현재 선택된 층에 해당하는 데이터 추출
-  let selectedData = floorDataList.find((f) => f.floor === selectedFloor)
-
-  // ----------------------
-  // IconBox 관련
-  // ----------------------
-  const [selectedIcon, setSelectedIcon] = useState(null)
+  const selectedData = floorDataList.find((f) => f.floor === selectedFloor)
 
   return (
     <div className="relative h-full w-full">
       {/* 지도 렌더링 */}
-      {selectedData && <MapBoxMap mode={mode} mapImageUrl={selectedData.image_url} />}
+      {selectedData && (
+        <MapBoxMap
+          mode={mode}
+          mapImageUrl={selectedData.image_url}
+          beaconList={selectedData.beacon_list}
+          edgeList={selectedData.edge_list}
+          selectedIcon={selectedIcon}
+        />
+      )}
 
       {/* 층 선택 UI */}
       <FloorNavigator
