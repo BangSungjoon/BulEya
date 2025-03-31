381e9ddcb401fbbd38d035eb81b00c241af876fa
권정우 <kiw970923@naver.com>
2025-04-01 08:34:27 +0900

    FEAT: 앱 실행 중 주변 비콘 지속적으로 스캔해 현재 위치 갱신
    - bug: 푸시 알림 터치하여 앱 실행 시 비콘 스캔하지 않음
    
    jira ref: #S12P21A204-150

--- mobile/app/src/main/java/com/ssafy/jangan_mobile/store/FireNotificationStore.kt
+++ mobile/app/src/main/java/com/ssafy/jangan_mobile/store/FireNotificationStore.kt
@@ -5,13 +5,14 @@ import androidx.lifecycle.MutableLiveData
 import com.ssafy.jangan_mobile.service.dto.FireNotificationDto
 
 object FireNotificationStore {
-    //var fireNotificationDto: FireNotificationDto? = null
-
     private val _fireNotificationDto = MutableLiveData<FireNotificationDto?>()
     val fireNotificationDto: LiveData<FireNotificationDto?> = _fireNotificationDto
 
     private val _currentNotificationBeaconCode = MutableLiveData<Int?>()
     val currentNotificationBeaconCode = _currentNotificationBeaconCode
+
+    private val _currentLocationStationId = MutableLiveData<Int?>()
+    val currentLocationStationId = _currentLocationStationId
 
     private val _currentLocationBeaconCode = MutableLiveData<Int?>()
     val currentLocationBeaconCode = _currentLocationBeaconCode
@@ -21,6 +22,9 @@ object FireNotificationStore {
     }
     fun setCurrentNotificationBeaconCode(beaconCode: Int?){
         _currentNotificationBeaconCode.value = beaconCode
+    }
+    fun setCurrentLocationStationId(stationId: Int?){
+        _currentLocationStationId.value = stationId
     }
     fun setCurrentLocationBeaconCode(beaconCode: Int?){
         _currentLocationBeaconCode.value = beaconCode
