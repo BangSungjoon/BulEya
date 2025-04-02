5ab027ee11116e6313602d4a29e8cb139f80da15
gmltn0907 <gmltn0907@naver.com>
2025-04-02 16:32:02 +0900

    FIX : 좌표 더블로 수정
    jira ref: #S12P21A204-210

--- mobile/app/src/main/java/com/ssafy/jangan_mobile/model/PixelLatLng.kt
+++ mobile/app/src/main/java/com/ssafy/jangan_mobile/model/PixelLatLng.kt
@@ -1,7 +1,7 @@
 package com.ssafy.jangan_mobile.model
 
 data class PixelLatLng(
-    val x: Int,
-    val y: Int,
+    val x: Double, // 경도 (Longitude)
+    val y: Double, // 위도 (Latitude)
     val floor: Int
 )
